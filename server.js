9d3a5107a684deb36829d5d4bab1841ac21ca932
Aarón Rodríguez-Barbero González <48285149+Aharon1377@users.noreply.github.com>
2020-12-12 12:37:00 +0100

    cors angular

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
 // Serve only the static files form the dist directory
 app.use(express.static('/app/dist/SIGETGrupo2Cliente'));
 
