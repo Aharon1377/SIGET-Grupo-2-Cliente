e913dfc2317cb7b00a7f1f447b8f00e4fb3ef457
Aarón Rodríguez-Barbero González <48285149+Aharon1377@users.noreply.github.com>
2020-12-12 14:09:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });
+const cors = require("cors");
+app.use(cors());
 
 // Serve only the static files form the dist directory
 app.use(express.static('/app/dist/SIGETGrupo2Cliente'));
